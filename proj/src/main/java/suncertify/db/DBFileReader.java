f52729f7750068e8348040e030bc2336943c5f3c
neomonkeus <neomonkeus@gmail.com>
2014-12-17 17:24:45 +0000

    Crude implementation for reading records. Needs looping logic to read all the records and way to map fields to data.

--- proj/src/main/java/suncertify/db/DBFileReader.java
+++ proj/src/main/java/suncertify/db/DBFileReader.java
@@ -36,21 +36,23 @@ class Header{
 	private int recordLength;
 	private short numFields;
 	private Field[] fields;
+	private Record[] records;
 	private RandomAccessFile raFile;
 	
 	Header(RandomAccessFile raFile) throws IOException{
 		this.raFile = raFile;
 		readHeader();
 		readFieldInfo();
+		readRecords();
 	}
 	
 	
 	private void readHeader() throws IOException{
-		recordLength = raFile.readInt();
 		magicCookie = raFile.readInt();
+		recordLength = raFile.readInt();
 		numFields = raFile.readShort();
 		System.out.println("Magic Cookie: " + magicCookie);
-		System.out.println("Record Lenth: " + recordLength);
+		System.out.println("Records total size: " + recordLength);
 		System.out.println("Num fields: " + numFields);
 	}
 	
@@ -59,30 +61,99 @@ class Header{
 		System.out.println("Reading Fields");
 		for(int i = 0; i < numFields; i++){
 			Field field = new Field();
-			field.namelenght = raFile.readShort();
-			byte[] name = new byte[field.namelenght];
-			raFile.read(name);
-			field.name = new String(name, "US-ASCII" );
-			field.size = raFile.readShort();
+			field.readField(raFile);
 			fields[i] = field; 
-			System.out.println(field.toString());
 		}
+		Record.setFields(fields);
+	}
+	
+	private void readRecords() throws IOException{
+		records = new Record[1];
+		Record r = new Record();
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
+		r.readRecord(raFile);
+		System.out.println(r);
 	}
 	
 }
 
 class Field{
-	short namelenght;
+	short namelength;
 	String name;
-	short size;
+	short dataLength;
+	int totalSize;
 	
-	@Override
-	public String toString() {
+	byte[] rawData;
+	String data;
+
+	public void readField(RandomAccessFile raFile) throws IOException{
+		namelength = raFile.readShort();
+		byte[] rawname = new byte[namelength];
+		raFile.read(rawname);
+		name = new String(rawname, "US-ASCII" );
+		
+		dataLength = raFile.readShort();
+		rawData = new byte[dataLength];
+
+		totalSize = namelength + name.length() + dataLength;
+	}
+	
+	public void readFieldData(RandomAccessFile raFile) throws IOException{
+		raFile.readFully(rawData);
+		data = new String(rawData, "US-ASCII" );
+	}
+	
+	
+	public String fieldData(){
+		return name + ":" + data.trim();
+	}
+	
+	public String fieldDescription() {
 		StringBuilder s = new StringBuilder();
 		s.append("Field:[");
 		s.append(name);
 		s.append(":");
-		s.append(size);
+		s.append(dataLength);
+		s.append("]");
+		return s.toString();
+	}
+}
+
+class Record{
+	boolean isdeleted = false;
+	static Field[] fields;
+	
+	public static void setFields(Field[] fields){
+		Record.fields = fields;
+	}
+	
+	public void readRecord(RandomAccessFile raFile) throws IOException{
+		isdeleted = raFile.readByte() != 0;
+		for(Field f : fields){
+			f.readFieldData(raFile);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("Record[deleted:");
+		s.append(isdeleted);
+		s.append("[");
+		for(Field f : fields){
+			s.append(f.fieldData());
+		}
 		s.append("]");
 		return s.toString();
 	}
